2e0b34702521f110884e0658f67aa70d4fc22ea3
Mbaye faye <mbayefaye@github.com>
2019-09-01 16:14:14 -0400

    add color helper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
-
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedColors[4]} />
-    </div>
-  );
+import { generatePalette } from "./ColorHelper";
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+    return (
+      <div className="App">
+        <Palette {...seedColors[4]} />
+      </div>
+    );
+  }
 }
 
 export default App;
