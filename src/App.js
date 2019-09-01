1e28bf7c1e0f244af64eeac696f6c9373b3cc806
Mbaye faye <mbayefaye@github.com>
2019-09-01 18:44:03 -0400

    add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./ColorHelper";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <h1>Palette list goes here</h1>}
+        ></Route>
+        <Route
+          exact
+          path="/palette/:id"
+          render={() => <h1>individual palette goes here</h1>}
+        ></Route>
+      </Switch>
+      /* <div className="App">
         <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      </div>*/
     );
   }
 }
