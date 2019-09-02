ad27bed72ccd973c6d0c960a637ff820d3312898
Mbaye faye <mbayefaye@github.com>
2019-09-02 12:19:08 -0400

    finish styling minipalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -3,7 +3,7 @@ import { withStyles } from "@material-ui/styles";
 
 const styles = {
   root: {
-    border: "1px solid red",
+    border: "1px solid grey",
     borderRadius: "5px ",
     padding: "0.5rem",
     position: "relative",
@@ -13,7 +13,11 @@ const styles = {
     }
   },
   colors: {
-    backgroundColor: "grey"
+    backgroundColor: "#dea1e4",
+    height: "150px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden"
   },
   title: {
     display: "flex",
@@ -28,13 +32,28 @@ const styles = {
   emoji: {
     marginLeft: "0.5rem",
     fontSize: "1.5rem"
+  },
+  miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-3.5px"
   }
 };
 function minipalette(props) {
-  const { classes, paletteName, emoji } = props;
+  const { classes, paletteName, emoji, colors } = props;
+  const miniColorBoxes = colors.map(color => (
+    <div
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+      key={color.name}
+    ></div>
+  ));
   return (
     <div className={classes.root}>
-      <div className={classes.colors}></div>
+      <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>{" "}
       </h5>
